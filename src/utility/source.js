4e053023dc8887ca40c7d4f4702c7520495794a4
Dima <aristov6569@gmail.com>
2020-07-05 12:45:32 +0500

    Cehck 1.2

--- src/utility/source.js
+++ src/utility/source.js
@@ -1,6 +1,6 @@
-export const randomNumber = () => {
-  const min = Math.ceil(min);
-  const max = Math.floor(max);
+export const randomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
