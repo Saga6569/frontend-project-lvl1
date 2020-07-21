8ff10f31b802332df97c148b4ec13d57ec95d1fa
saga6569 <aristov6569@gmail.com>
2020-07-21 18:53:33 +0200

    correction 7.0

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const gameEngine = (description, game) => {
   console.log(`Hello ${name}!`);
   console.log(description);
   for (let i = 1; i <= 3; i += 1) {
-    const result = game();
-    console.log(result.question);
-    const userResponse = readlineSync.question('Your answer: ');
-    if (String(result.answer) === userResponse) {
+    const { answer, question } = game();
+    console.log(question);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (String(answer) === userAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`"${userResponse}" is wrong answer ;(. Correct answer was "${result.answer}" .`);
+      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}" .`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
